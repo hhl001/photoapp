683f4359744ab05b2b0d4206d772579ec096e9dd
Henry Huang <henryhuang@Henrys-MacBook-Pro.local>
2025-01-21 22:42:37 -0500

    Inital commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+import image1 from "./images/cattle.jpeg"; // Import the image
+import image2 from "./images/duck.jpeg"; // Import the image
+import image3 from "./images/sheep.jpeg"; // Import the image
+
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "20px",
+        justifyContent: "center",
+        marginTop: "20px",
+      }}
+    >
+      <img
+        src={image1} // Use the imported image
+        alt="Image 1"
+        style={{ width: "150px", height: "150px", borderRadius: "10px" }}
+      />
+      <img
+        src={image2} // Use the imported image
+        alt="Image 2"
+        style={{ width: "150px", height: "150px", borderRadius: "10px" }}
+      />
+      <img
+        src={image3} // Use the imported image
+        alt="Image 3"
+        style={{ width: "150px", height: "150px", borderRadius: "10px" }}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/custom.d.ts
@@ -0,0 +1,9 @@
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
+declare module "*.jpeg" {
+  const value: string;
+
+  export default value;
+}
