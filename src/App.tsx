0397b0d759a4ab5882da0c503ab170494e88f6cf
hhl001 <hhldecade@gmail.com>
2025-01-23 15:51:41 -0500

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
 import image1 from "./images/cattle.jpeg"; // Import the image
-import image2 from "./images/duck.jpeg"; // Import the image
-import image3 from "./images/sheep.jpeg"; // Import the image
+import image2 from "./images/sheep.jpeg"; // Import the image
+import image3 from "./images/duck.jpeg"; // Import the image
+import image4 from "./images/duck02.jpeg"; // Import the image
 
 function App() {
   return (
@@ -27,6 +28,11 @@ function App() {
       <img
         src={image3} // Use the imported image
         alt="Image 3"
+        style={{ width: "150px", height: "150px", borderRadius: "10px" }}
+      />
+      <img
+        src={image4} // Use the imported image
+        alt="Image 4"
         style={{ width: "150px", height: "150px", borderRadius: "10px" }}
       />
     </div>
