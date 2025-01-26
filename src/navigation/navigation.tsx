9f234301b9dc27fa371d99ac98b5b1a6679f867d
hhl001 <hhldecade@gmail.com>
2025-01-25 22:55:20 -0500

    Added videos

--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -93,19 +93,41 @@ function DemoPageContent({ pathname }: { pathname: string }) {
   const [open, setOpen] = React.useState(false);
   const [currentIndex, setCurrentIndex] = React.useState(0);
   const images = [image1, image2, image3, image4]; // Array of images
+  const videos = [
+    {
+      type: "video",
+      src: "https://www.youtube.com/embed/_-gbT5qxQr8?start=103",
+      thumbnail: "https://img.youtube.com/vi/_-gbT5qxQr8/hqdefault.jpg",
+    },
+    {
+      type: "video",
+      src: "https://www.youtube.com/embed/Kwh9GuZRSN8",
+      thumbnail: "https://img.youtube.com/vi/Kwh9GuZRSN8/hqdefault.jpg",
+    },
+  ];
 
   const handleOpen = (index: number) => {
     setCurrentIndex(index);
     setOpen(true);
   };
 
-  const handleNext = () => {
+  const handleNextImage = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length); // Loop to the first image after the last one
   };
 
-  const handlePrev = () => {
+  const handlePrevImage = () => {
     setCurrentIndex((prevIndex) =>
       prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    ); // Loop to the last image if at the first one
+  };
+
+  const handleNextVideo = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % videos.length); // Loop to the first image after the last one
+  };
+
+  const handlePrevVideo = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? videos.length - 1 : prevIndex - 1
     ); // Loop to the last image if at the first one
   };
 
@@ -171,7 +193,7 @@ function DemoPageContent({ pathname }: { pathname: string }) {
               }}
             >
               <button
-                onClick={handlePrev}
+                onClick={handlePrevImage}
                 style={{
                   background: "none",
                   border: "none",
@@ -183,7 +205,130 @@ function DemoPageContent({ pathname }: { pathname: string }) {
                 Previous
               </button>
               <button
-                onClick={handleNext}
+                onClick={handleNextImage}
+                style={{
+                  background: "none",
+                  border: "none",
+                  cursor: "pointer",
+                  padding: "8px 16px",
+                  fontSize: "16px",
+                }}
+              >
+                Next
+              </button>
+            </Box>
+          </Box>
+        </Dialog>
+      </div>
+    );
+  } else if (pathname === "/videos") {
+    return (
+      <div
+        style={{
+          display: "flex",
+          gap: "20px",
+          justifyContent: "center",
+          marginTop: "20px",
+        }}
+      >
+        {/* Render all items */}
+        {videos.map((item, index) => (
+          <div
+            key={index}
+            style={{ cursor: "pointer", textAlign: "center" }}
+            onClick={() => handleOpen(index)}
+          >
+            {
+              <div style={{ position: "relative" }}>
+                {/* Video thumbnail */}
+                <img
+                  src={item.thumbnail}
+                  alt={`Video ${index + 1}`}
+                  style={{
+                    width: "150px",
+                    height: "150px",
+                    borderRadius: "10px",
+                    objectFit: "cover",
+                  }}
+                />
+                {/* Play button overlay */}
+                <div
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    backgroundColor: "rgba(0, 0, 0, 0.6)",
+                    borderRadius: "50%",
+                    padding: "10px",
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="white"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                  >
+                    <path d="M8 5v14l11-7z" />
+                  </svg>
+                </div>
+              </div>
+            }
+          </div>
+        ))}
+
+        {/* Dialog for enlarged content */}
+        <Dialog open={open} onClose={handleClose} maxWidth="lg">
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              p: 2,
+            }}
+          >
+            {
+              // YouTube video
+              <iframe
+                width="800"
+                height="450"
+                src={videos[currentIndex].src}
+                title="YouTube video"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                style={{
+                  maxWidth: "90vw",
+                  maxHeight: "90vh",
+                }}
+              ></iframe>
+            }
+
+            {/* Navigation buttons */}
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "100%",
+                mt: 2,
+              }}
+            >
+              <button
+                onClick={handlePrevVideo}
+                style={{
+                  background: "none",
+                  border: "none",
+                  cursor: "pointer",
+                  padding: "8px 16px",
+                  fontSize: "16px",
+                }}
+              >
+                Previous
+              </button>
+              <button
+                onClick={handleNextVideo}
                 style={{
                   background: "none",
                   border: "none",
